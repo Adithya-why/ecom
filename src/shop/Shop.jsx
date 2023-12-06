6900d5679a9e3a97b694c0c4e14ab41a7e6a2ac2
Adi <adithiyans123@gmail.com>
2023-12-06 16:14:04 +0530

    specified the arrangement of items in grid

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -9,7 +9,10 @@ export default function Shop({ handleclick }){
         <div className=" bg-black h-full text-gray-400 flex flex-col">
             <h1 className="text-4xl text-center font-bold mt-3 ">Shop</h1>
 
-            <div className="itemsgrid">
+            <div className="itemsgrid mt-6">
+                <Item name={'Computer'} price={'R$10000'}/>
+                <Item name={'Computer'} price={'R$10000'}/>
+                <Item name={'Computer'} price={'R$10000'}/>
                 <Item name={'Computer'} price={'R$10000'}/>
             </div>
 
