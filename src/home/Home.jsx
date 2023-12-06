01d9a16ec77b589098c55cf3088ea0758708e476
Adi <adithiyans123@gmail.com>
2023-12-06 15:53:14 +0530

    initial created the home  page with navabar

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home(){
         <div className=" bg-slate-500 flex flex-col items-center mt-10 text-gray-200" >
             <h2 className="text-6xl mt-3">Totally real store <span className="italic text-xl">btw</span></h2>
 
-            <h4 className="mt-3">Real products at real prices</h4>
+            <h4 className="mt-3">Real products at Real prices</h4>
 
             <div className='text-xl mt-6 bg-orange-500 p-4 mb-6 rounded-lg'>
                 <Link to={'/shop'}>Check out our products</Link>
