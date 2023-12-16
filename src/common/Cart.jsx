21cc67e3f803d4146bbc5da93ed1739df815fdc7
Adi <adithiyans123@gmail.com>
2023-12-16 08:48:01 +0530

    added total

--- src/common/Cart.jsx
+++ src/common/Cart.jsx
@@ -1,11 +1,15 @@
 export default function Cart({ cart }){
-   
+   //basically create a card for every element in cart
+   //also calculates total
     let products = [];
+    let total = 0;
     for(let i =0;i<cart.length;i++){
         products.push(<CartItem item={cart[i]} key={i}/>)
+        total += cart[i].price*cart[i].val;
         
     }
    
+
 
     return(
         <div className="text-white">
@@ -22,6 +26,22 @@ export default function Cart({ cart }){
             <div className="text-white flex items-center justify-center flex-col gap-6">
                 {products}
             </div>
+
+            <div className="flex items-center justify-center mb-10 mt-5">
+                <div className=" bg-slate-700 w-2/4 h-24 cartitem ">
+                <div>Total</div>
+                    <div></div>
+                    
+                    <div>{'$'+total}</div>
+                
+                </div>
+            </div>
+
+            <div className=" flex items-center justify-center">
+                <button className=" bg-orange-500 p-4 rounded-md">Checkout and Pay</button>
+            </div>
+
+            
         </div>
     )
 }
@@ -34,7 +54,7 @@ function CartItem(item){
         <div className=" bg-slate-700 w-2/4 h-24 cartitem ">
             <div>{item.item.name}</div>
             <div>{item.item.val}</div>
-            <div>{item.item.price*item.item.val}</div>
+            <div>{'$'+parseInt(item.item.price*item.item.val)}</div>
 
             
         </div>
